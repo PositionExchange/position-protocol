57dfa3de1222644a046c190eac97c76b34f51004
Justin <justin.gun@position.exchange>
2021-09-24 22:08:12 +0700

    refactor: open reverse position

--- contracts/protocol/PositionHouse.sol
+++ contracts/protocol/PositionHouse.sol
@@ -13,6 +13,11 @@ contract PositionHouse is Initializable, ReentrancyGuardUpgradeable, OwnableUpgr
     using Position for Position.Data;
     //    enum Side {LONG, SHORT}
 
+    enum PnlCalcOption {
+        TWAP,
+        SPOT_PRICE,
+        ORACLE
+    }
     struct PositionResp {
 
         Position.Data position;
@@ -51,12 +56,15 @@ contract PositionHouse is Initializable, ReentrancyGuardUpgradeable, OwnableUpgr
         //check input
         address _trader = _msgSender();
         //TODO check is new Position
-        bool isNewPosition = isNewPosition(_positionManager, _trader);
+        //        bool isNewPosition = isNewPosition(_positionManager, _trader);
+        Position.Data memory oldPosition = getPosition(address(_positionManager), _trader);
         PositionResp memory positionResp;
-        if (isNewPosition) {
+        if (oldPosition.size == 0 || oldPosition.side == _side) {
             positionResp = increasePosition(_positionManager, _side, _size, _leverage);
         } else {
+
             // TODO adjust old position
+            positionResp = openReversePosition(_positionManager, _side, _size, _leverage);
         }
         // update position sate
         positionMap[address(_positionManager)][_trader].update(
@@ -75,52 +83,80 @@ contract PositionHouse is Initializable, ReentrancyGuardUpgradeable, OwnableUpgr
 
     }
 
-    function adjustPosition() internal returns (PositionResp memory positionResp) {
-
-    }
-
     function increasePosition(
         IPositionManager _positionManager,
         Position.Side _side,
         uint256 _size,
         uint256 _leverage
-    ) internal returns (PositionResp memory positionResp) {
-        address trader = _msgSender();
-        Position.Data memory oldPosition = getPosition(address(_positionManager), trader);
+    ) public returns (PositionResp memory positionResp) {
+        address _trader = _msgSender();
+        Position.Data memory oldPosition = getPosition(address(_positionManager), _trader);
         positionResp.exchangedPositionSize = _positionManager.openMarketPosition(_size * _leverage, _side == Position.Side.LONG);
-        uint256 _newSize = oldPosition.size + positionResp.exchangedPositionSize / _leverage;
-        console.log("PositionHouse. New Size: ", _newSize);
-        uint256 _currentPrice = _positionManager.getPrice();
-        uint256 increaseMarginRequirement = _size * _currentPrice / _leverage;
-        // TODO update function latestCumulativePremiumFraction
-        (uint256 remainMargin, uint256 fundingPayment, uint256 latestCumulativePremiumFraction) =
-        calcRemainMarginWithFundingPayment(oldPosition.margin, increaseMarginRequirement);
-        // TODO function getUnrealizedPnl
-        (int256 unrealizedPnl) = getUnrealizedPnl();
+        if (positionResp.exchangedPositionSize > 0) {
+            uint256 _newSize = oldPosition.size + positionResp.exchangedPositionSize / _leverage;
+            uint256 _currentPrice = _positionManager.getPrice();
+            uint256 increaseMarginRequirement = _size * _currentPrice / _leverage;
+            // TODO update function latestCumulativePremiumFraction
+            (uint256 remainMargin, uint256 fundingPayment, uint256 latestCumulativePremiumFraction) =
+            calcRemainMarginWithFundingPayment(oldPosition.margin, int256(increaseMarginRequirement));
+            // TODO function getUnrealizedPnl
+            (,int256 unrealizedPnl) = getPositionNotionalAndUnrealizedPnl(_positionManager, _trader, PnlCalcOption.SPOT_PRICE);
 
-        // update positionResp
-        positionResp.exchangedQuoteAssetAmount = (_size * _leverage) * _currentPrice;
-        positionResp.unrealizedPnl = unrealizedPnl;
-        positionResp.marginToVault = increaseMarginRequirement;
-        positionResp.fundingPayment = fundingPayment;
-        positionResp.position = Position.Data(
-            _side,
-            _newSize,
-            remainMargin,
-            oldPosition.openNotional + positionResp.exchangedQuoteAssetAmount,
-            latestCumulativePremiumFraction,
-            block.number
-        );
+            // update positionResp
+            positionResp.exchangedQuoteAssetAmount = (_size * _leverage) * _currentPrice;
+            positionResp.unrealizedPnl = unrealizedPnl;
+            positionResp.marginToVault = increaseMarginRequirement;
+            positionResp.fundingPayment = fundingPayment;
+            positionResp.position = Position.Data(
+                _side,
+                _newSize,
+                remainMargin,
+                oldPosition.openNotional + positionResp.exchangedQuoteAssetAmount,
+                latestCumulativePremiumFraction,
+                block.number
+            );
+        }
     }
 
     function openReversePosition(
         IPositionManager _positionManager,
-        address _trader,
         Position.Side _side,
-        uint256 _openNotional,
+        uint256 _size,
         uint256 _leverage
     ) internal returns (PositionResp memory positionResp) {
-        (uint256 oldPositionNotional, uint256 unrealizedPnl) = getPositionNotionalAndUnrealizedPnl();
+        address _trader = _msgSender();
+
+        (uint256 oldPositionNotional, int256 unrealizedPnl) = getPositionNotionalAndUnrealizedPnl(_positionManager, _trader, PnlCalcOption.SPOT_PRICE);
+        PositionResp memory positionResp;
+        uint256 _currentPrice = _positionManager.getPrice();
+        // reduce position if old position is larger
+        uint256 newNotional = _size * _currentPrice * _leverage;
+
+        if (oldPositionNotional > newNotional) {
+            Position.Data memory oldPosition = getPosition(address(_positionManager), _trader);
+            positionResp.exchangedPositionSize = _positionManager.openMarketPosition(_size * _leverage, _side == Position.Side.LONG);
+            // margin required to reduce
+            uint256 reduceMarginRequirement = _size * _currentPrice / _leverage;
+            positionResp.realizedPnl = (unrealizedPnl * int256(positionResp.exchangedPositionSize)) / int256(oldPosition.size);
+            console.log("releaszedPnl %s", uint256(positionResp.realizedPnl));
+            console.log("reduceMarginRequirement %s", (reduceMarginRequirement));
+            console.log("old margin %s", (oldPosition.margin));
+            // update old position
+            (uint256 remainMargin, uint256 fundingPayment, uint256 latestCumulativePremiumFraction) =
+            calcRemainMarginWithFundingPayment(oldPosition.margin, (positionResp.realizedPnl - int256(reduceMarginRequirement)));
+            positionResp.position = Position.Data(
+                oldPosition.side,
+                oldPosition.size - _size,
+                remainMargin,
+                oldPosition.openNotional - newNotional,
+                0,
+                0
+            );
+            console.log("return remainMargin", remainMargin);
+            return positionResp;
+        }
+        // if new position is larger then close old and open new
+        return closeAndOpenReversePosition();
     }
 
     function getPosition(
@@ -144,16 +180,42 @@ contract PositionHouse is Initializable, ReentrancyGuardUpgradeable, OwnableUpgr
         unrealizedPnl = 0;
     }
 
-    function calcRemainMarginWithFundingPayment(uint256 oldPositionMargin, uint256 newPositionMargin) internal view returns (uint256 remainMargin, uint256 fundingPayment, uint256 latestCumulativePremiumFraction){
-        remainMargin = oldPositionMargin + newPositionMargin;
+    function closeAndOpenReversePosition() internal view returns (PositionResp memory positionResp) {
+
+    }
+
+    function calcRemainMarginWithFundingPayment(uint256 oldPositionMargin, int256 deltaMargin) internal view returns (uint256 remainMargin, uint256 fundingPayment, uint256 latestCumulativePremiumFraction){
+        remainMargin = uint256(int256(oldPositionMargin) + deltaMargin);
         fundingPayment = 0;
         latestCumulativePremiumFraction = 0;
     }
 
-    function getPositionNotionalAndUnrealizedPnl() internal view returns (uint256 oldPositionNotional, uint256 unrealizedPnl){
-        oldPositionNotional = 0;
-        unrealizedPnl = 0;
+    function getPositionNotionalAndUnrealizedPnl(
+        IPositionManager positionManager,
+        address _trader,
+        PnlCalcOption _pnlCalcOption
+    ) public view returns
+    (
+        uint256 positionNotional,
+        int256 unrealizedPnl
+    ){
+        Position.Data memory position = getPosition(address(positionManager), _trader);
+        uint256 oldPositionNotional = position.openNotional;
+        if (_pnlCalcOption == PnlCalcOption.TWAP) {
+            // TODO get twap price
+        } else if (_pnlCalcOption == PnlCalcOption.SPOT_PRICE) {
+            positionNotional = positionManager.getPrice() * position.size * (position.openNotional/position.margin);
+        } else {
+            // TODO get oracle price
+        }
+        if (position.side == Position.Side.LONG) {
+            unrealizedPnl = int256(positionNotional) - int256(oldPositionNotional);
+        } else {
+            unrealizedPnl = int256(oldPositionNotional) - int256(positionNotional);
+        }
+
     }
+
 
     function isNewPosition(
         IPositionManager _positionManager,
--- test/unit/PositionHouse.spec.ts
+++ test/unit/PositionHouse.spec.ts
@@ -24,8 +24,10 @@ interface PositionData {
 
 describe("PositionHouse", () => {
     let positionHouse: PositionHouse;
+    let trader: any;
     let positionManager: PositionManager;
     beforeEach(async () => {
+        [trader] = await ethers.getSigners()
         const positionManagerFactory = await ethers.getContractFactory("PositionManager")
         // BTC-USD Perpetual, initial price is 5000
         // each pip = 0.01
@@ -36,6 +38,42 @@ describe("PositionHouse", () => {
     })
 
     describe('openMarketPosition', async () => {
+        const openMarketPosition = async ({
+                                              size,
+                                              leverage,
+                                              side,
+                                              trader,
+                                              expectedMargin,
+                                              expectedNotional,
+                                              expectedSize,
+                                              price = 5000
+                                          }: {
+            size: BigNumber,
+            leverage: number,
+            side: number,
+            trader: string,
+            expectedMargin?: BigNumber,
+            expectedNotional?: BigNumber | string,
+            expectedSize?: BigNumber,
+            price?: number
+        }) => {
+            await positionHouse.openMarketPosition(
+                positionManager.address,
+                side,
+                size,
+                leverage,
+            )
+            const positionInfo = await positionHouse.getPosition(positionManager.address, trader) as unknown as PositionData;
+            console.log("positionInfo", positionInfo)
+            const openNotional = positionInfo.openNotional.div('10000').toString()
+            expectedNotional = expectedNotional && expectedNotional.toString() || size.mul(price).mul(leverage.toString()).toString()
+
+            expect(positionInfo.size.toString()).eq(expectedSize || size.toString())
+            expect(openNotional).eq(expectedNotional)
+            expectedMargin && expect(positionInfo.margin.div('10000').toString()).eq(expectedMargin.toString())
+        }
+
+
         it('should open market a position', async function () {
             const [trader] = await ethers.getSigners()
             const size = toWeiBN('1')
@@ -46,19 +84,80 @@ describe("PositionHouse", () => {
                 toWeiBN('10'),
                 true
             );
-            await positionHouse.openMarketPosition(
-                positionManager.address,
-                SIDE.SHORT,
-                size,
-                leverage,
-            )
-            const positionInfo = await positionHouse.getPosition(positionManager.address, trader.address) as unknown as PositionData;
-            console.log(positionInfo)
-            const openNotional = positionInfo.openNotional.div('10000').toString()
-            const expectedNotional = size.mul(5000).mul(leverage.toString()).toString()
-            expect(positionInfo.size.toString()).eq(size.toString())
-            expect(openNotional).eq(expectedNotional)
-            expect(positionInfo.margin.div('10000').toString()).eq(toWeiWithString('500'))
+
+            await openMarketPosition({
+                    size: size,
+                    leverage: leverage,
+                    side: SIDE.SHORT,
+                    trader: trader.address
+                }
+            );
+
+
+        });
+
+        describe('get PnL', function () {
+            it('should get PnL market', async function () {
+
+                await positionManager.openLimitPosition(
+                    priceToPip(5000),
+                    '20',
+                    true
+                );
+                await openMarketPosition({
+                    size: BigNumber.from('2'),
+                    side: SIDE.SHORT,
+                    trader: trader.address,
+                    leverage: 10
+                });
+                const positionNotionalAndPnL = await positionHouse.getPositionNotionalAndUnrealizedPnl(
+                    positionManager.address,
+                    trader.address,
+                    1
+                )
+                console.log("positionNotionalAndPnL ", positionNotionalAndPnL.toString())
+                expect(positionNotionalAndPnL.unrealizedPnl).eq(0)
+
+            });
+        });
+
+        describe('should reduce current position', async function () {
+            it('pnl = 0', async function () {
+                await positionManager.openLimitPosition(
+                    priceToPip(5000),
+                    '20',
+                    true
+                );
+                await openMarketPosition({
+                    size: BigNumber.from('2'),
+                    leverage: 10,
+                    side: SIDE.SHORT,
+                    trader: trader.address
+                })
+                await positionManager.openLimitPosition(
+                    priceToPip(5000),
+                    toWeiBN('10'),
+                    false
+                );
+                await openMarketPosition({
+                    size: BigNumber.from('1'),
+                    side: SIDE.LONG,
+                    leverage: 10,
+                    trader: trader.address,
+                    expectedSize: BigNumber.from('1'),
+                    expectedNotional: BigNumber.from('50000'),
+                    expectedMargin: BigNumber.from('500')
+                })
+            });
+
+
+            it('should pnl > 0', async function () {
+
+            });
+
+            it('should pnl < 0', async function () {
+
+            });
         });
     })
 
