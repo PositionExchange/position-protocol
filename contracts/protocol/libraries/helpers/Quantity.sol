bc5f4371f6b42a34feb20d5aea67cdfa6931c554
Justin Position <justin@position.exchange>
2022-01-13 23:36:51 +0700

    Quantity.abs to abs()

--- contracts/protocol/libraries/helpers/Quantity.sol
+++ contracts/protocol/libraries/helpers/Quantity.sol
@@ -6,7 +6,7 @@ library Quantity {
         uint256 openNotional,
         uint256 oldPQuantity
     ) internal pure returns (uint256) {
-        return (Quantity.abs(quantity) * openNotional) / oldPQuantity;
+        return (abs(quantity) * openNotional) / oldPQuantity;
     }
 
     function getPartiallyLiquidate(
