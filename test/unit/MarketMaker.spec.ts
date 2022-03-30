2c329f7272855599d74519414d2488596fb74e9f
JustinPosition <justin.gun@position.exchange>
2022-03-30 16:37:43 +0700

    move deploy position house to a function

---
+++ test/unit/MarketMaker.spec.ts
@@ -0,0 +1,13 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {PositionHouse, PositionManager} from "../../typeChain";
+
+describe('Market Maker', function () {
+    let deployer: SignerWithAddress
+    let positionManager: PositionManager
+    let positionHouse: PositionHouse
+
+    beforeEach(async () => {
+
+    })
+
+});
