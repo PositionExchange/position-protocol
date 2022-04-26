8a461e579fdf651d9f947de2e8d0e4817111c5c2
DE <desert.eagle@position.exchange>
2022-04-26 14:49:12 +0800

    add unit test

--- test/unit/PositionHouse_02.spec.ts
+++ test/unit/PositionHouse_02.spec.ts
@@ -5859,5 +5859,37 @@ describe("PositionHouse_02", () => {
             console.log(exchangedQuoteAmount.toString())
             expect(exchangedQuoteAmount.toString()).eq("992")
         })
+
+        it("should fill order correctly when single slot is full buy is wrong", async () => {
+            await openLimitPositionAndExpect({
+                limitPrice: 5000,
+                side: SIDE.LONG,
+                leverage: 10,
+                quantity: BigNumber.from('10'),
+                _trader: trader1,
+                skipCheckBalance: true
+            })
+
+            await openLimitPositionAndExpect({
+                limitPrice: 4900,
+                side: SIDE.LONG,
+                leverage: 10,
+                quantity: BigNumber.from('10'),
+                _trader: trader2,
+                skipCheckBalance: true
+            })
+
+            await cancelLimitOrder(positionManager.address, trader2, '1', '490000')
+
+            await expect(openMarketPosition({
+                    quantity: BigNumber.from('10'),
+                    leverage: 10,
+                    side: SIDE.LONG,
+                    trader: trader2.address,
+                    instanceTrader: trader2,
+                    _positionManager: positionManager,
+                }
+            )).to.be.revertedWith("11")
+        })
     })
 })
