cd75dfccc1a565017e17bbf69cc8082273f259d2
DE <desert.eagle@position.exchange>
2022-05-29 05:32:47 +0800

    update positionNotionalConfigProxy

--- contracts/protocol/PositionNotionalConfigProxy.sol
+++ contracts/protocol/PositionNotionalConfigProxy.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract PositionNotionalConfigProxy is Initializable {
+    bytes32 BTC_BUSD = "BTC_BUSD";
+    bytes32 BNB_BUSD = "BNB_BUSD";
+
     function getMaxNotional(bytes32 key, uint16 leverage) external returns (uint256){
-        if(key == "") { //BTC_BUSD hash
+        if(key == BTC_BUSD) { //BTC_BUSD hash
             if(leverage == 1) {
                 return 1_000_000_000_000;
             }else if(leverage == 2){
@@ -26,6 +29,24 @@ contract PositionNotionalConfigProxy is Initializable {
                 return 250_000;
             }
             return 50_000;
+        } else if (key == BNB_BUSD) { //BNB_BUSD hash
+            if(leverage == 1) {
+                return 30_000_000;
+            }else if(leverage == 2){
+                return 5_000_000;
+            }else if(leverage == 3){
+                return 2_000_000;
+            }else if(leverage == 4){
+                return 1_000_000;
+            }else if(leverage >= 5 && leverage <= 10){
+                return 500_000;
+            }else if(leverage > 10 && leverage <= 20){
+                return 100_000;
+            }else if(leverage > 20 && leverage <= 50){
+                return 50_000;
+            }else if(leverage > 50 && leverage <= 100){
+                return 10_000;
+            }
         }
         return 50_000;
     }
