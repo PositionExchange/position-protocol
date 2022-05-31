e6dd3ba953b48c2fcff2595ad5d1cd140953a33a
JustinPosition <justin.gun@position.exchange>
2022-05-31 19:04:01 +0700

    update notional for BTCBUSD pair

--- contracts/protocol/PositionNotionalConfigProxy.sol
+++ contracts/protocol/PositionNotionalConfigProxy.sol
@@ -12,17 +12,17 @@ contract PositionNotionalConfigProxy is Initializable {
             if(leverage == 1) {
                 return 1_000_000_000_000;
             }else if(leverage == 2){
-                return 600_000_000;
+                return 6_000_000;
             }else if(leverage == 3){
-                return 400_000_000;
+                return 4_000_000;
             }else if(leverage == 4){
-                return 200_000_000;
+                return 2_000_000;
             }else if(leverage >= 5 && leverage <= 10){
-                return 100_000_000;
+                return 60_000;
             }else if(leverage > 10 && leverage <= 20){
-                return 40_000_000;
+                return 40_000;
             }else if(leverage > 20 && leverage <= 50){
-                return 7_500_000;
+                return 30_000;
             }else if(leverage > 50 && leverage <= 100){
                 return 1_000_000;
             }else if(leverage > 100 && leverage <= 124){
