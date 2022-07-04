15233df67f9474e2abbeafd7bb82287991c36106
DE <desert.eagle@position.exchange>
2022-07-04 15:55:33 +0800

    update funding payment formula

--- deploy/migrations/004-deploy-chainlink-pricefeed.ts
+++ deploy/migrations/004-deploy-chainlink-pricefeed.ts
@@ -2,16 +2,16 @@ import {MigrationContext, MigrationDefinition} from "../types";
 import {ContractWrapperFactory} from "../ContractWrapperFactory";
 const migrations: MigrationDefinition = {
     getTasks: (context: MigrationContext) => ({
-        'deploy chain link price feed': async () => {
-            /**
-             * Currently no param
-             */
-            if (context.network == "qc") {
-                await context.factory.createChainlinkPriceFeedQc({})
-            } else {
-                await context.factory.createChainlinkPriceFeed({})
-            }
-        }
+        // 'deploy chain link price feed': async () => {
+        //     /**
+        //      * Currently no param
+        //      */
+        //     if (context.network == "qc") {
+        //         await context.factory.createChainlinkPriceFeedQc({})
+        //     } else {
+        //         await context.factory.createChainlinkPriceFeed({})
+        //     }
+        // }
     })
 }
 export default migrations;
