13ad9214f717d426fec9a04be48e051612913b84
DE <desert.eagle@position.exchange>
2022-07-15 19:43:58 +0800

    add script hot fix premium fraction

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,6 +8,8 @@ import {DeployDataStore} from "../deploy/DataStore";
 import {BUSD, BUSD_ADDRESS, POSI, POSI_ADDRESS} from "../constants";
 import {TransactionResponse} from "@ethersproject/abstract-provider";
 import {verifyContract} from "./utils";
+import {PositionHouse} from "../typeChain";
+import {readFileSync} from "fs";
 
 const DATA_STORE_FILE = {
     'usd-m': './deployData_mainnet.db',
@@ -73,5 +75,18 @@ task('verifyImp', 'Verify all implemented contracts', async (taskArgs: {stage: S
         })
     }
 }).addParam('type', 'Type of Perpetual Future Contract', 'usd-m')
+
+task('hotFixPremiumFraction', 'Update new premium fraction', async (taskArgs: {stage: Stage, type: string}, hre) => {
+    const db = new DeployDataStore(DATA_STORE_FILE[taskArgs.type || 'usd-m'])
+    const positionHouseAddress = await db.findAddressByKey(`PositionHouse`)
+    const positionHouse = await hre.ethers.getContractAt('PositionHouse', positionHouseAddress) as PositionHouse
+    const positionManagerAddresses = ['0x25a91e02d37df8d1d47ad92cfcf2b6d1f6528a92', '0x8f887d4b9957b82b2476864d55ecd32a1c3d8c15']
+    const arrayTrader = readFileSync('./listTraders.txt', 'utf8').split(/\r?\n/)
+    for (const managerAddress of positionManagerAddresses) {
+        const tx = await positionHouse.hotFixUpdateFundingRate(managerAddress, arrayTrader)
+        await tx.wait()
+        console.log("Update new funding rate success")
+    }
+})
 
 export default {}
